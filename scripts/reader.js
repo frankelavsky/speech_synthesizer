434cf4938708abb9ba573a57bab738a7f8ecefc7
Frank Elavsky <frankelavsky@gmail.com>
2021-10-09 16:55:48 -0400

    wip: add event listeners on window load

--- scripts/reader.js
+++ scripts/reader.js
@@ -1,3 +1,9 @@
-import {synthesize} from './synthesize'
-
-document.body.addEventListener('mouseover', synthesize)
+window.onload = function() {
+    document.body.addEventListener('mouseover', showButton)
+    document.body.addEventListener('keydown', (e) => {
+        if (e.code === 'Space' || e.keyCode === 0 || e.keyCode === 32) {
+            e.preventDefault()
+            synthesize(currentText)
+        }
+    })
+};
